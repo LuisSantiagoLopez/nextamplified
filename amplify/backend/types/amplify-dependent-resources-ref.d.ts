47ed769ccf96e31dd511421cb5b0d231ba53aa58
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-12-13 00:09:52 -0600

    Install @aws-amplify/ui-react and update build settings

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "nextamplified": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
